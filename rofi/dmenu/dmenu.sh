14274a8fb1c53c6a2b7de0ce192c9c89e7d6b6d0
Musashi7818 <davimenesesk0805@gmail.com>
2026-06-17 01:51:15 -0300

    Update dmenu.sh

--- rofi/dmenu/dmenu.sh
+++ rofi/dmenu/dmenu.sh
@@ -1,10 +1,10 @@
 !#/bin/bash
 
-option=$(printf "Configuration\nCustomization\nTerminals\nRun" | rofi -dmenu -i -p "Options" -theme "$HOME/.config/rofi/dmenu/theme/theme.rasi")
+option=$(printf " Configuration\n Customization\n Terminals\n Run" | rofi -dmenu -i -p "Options" -theme "$HOME/.config/rofi/dmenu/theme/theme.ra>
 
 case "$option" in
-        "Configuration") sh -c "$HOME/.config/rofi/dmenu/commands/configuration.sh";;
-        "Customization") sh -c "$HOME/.config/rofi/dmenu/commands/customization.sh";;
-        "Run") sh -c "rofi -show run -theme "$HOME/.config/rofi/dmenu/theme/theme.rasi" ";;
-        "Terminals") sh -c "$HOME/.config/rofi/dmenu/commands/terminals.sh";;
+        " Configuration") sh -c "$HOME/.config/rofi/dmenu/commands/configuration.sh";;
+        " Customization") sh -c "$HOME/.config/rofi/dmenu/commands/customization.sh";;
+        " Run") sh -c "rofi -show run -theme "$HOME/.config/rofi/dmenu/theme/theme.rasi" ";;
+        " Terminals") sh -c "$HOME/.config/rofi/dmenu/commands/terminals.sh";;
 esac
