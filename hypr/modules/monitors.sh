32f1ba286f08a0d0d7338eb11b8fc374940656f5
Musashi7818 <davimenesesk0805@gmail.com>
2026-06-17 00:04:03 -0300

    Add monitor configuration to monitors.sh

---
+++ hypr/modules/monitors.sh
@@ -0,0 +1,8 @@
+##############
+## MONITORS ##
+##############
+
+monitor = eDP-1, 1366x768@59.99900,0x0,1
+monitor = HDMI-A-1,1920x1080@120.00,1366x0,1
+layerrule = match:namespace swaync-control-center, blur on,
+layerrule = match:namespace swaync-control-center, ignore_alpha 0.2,
